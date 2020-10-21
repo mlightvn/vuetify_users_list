a45f48b145d6fd5494fa9bf64d5c0faeee616a11
Nguyen Ngoc Nam <thietlalung@yahoo.com.vn>
2020-10-21 19:33:17 +0900

    layout

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
      <v-footer app>
-          &copy;2020 — <strong><a href="https://github.com/namtenten" target="_blank" class="success--text mr-1">Nguyen Ngoc Nam</a></strong> | <a class="ml-1" href="https://www.paypal.me/rakujin" target="_blank"><img src="/images/Donate-PayPal-green.svg" alt="Donate"></a>
+          &copy;2020 — <strong><a href="https://github.com/namtenten" target="_blank" class="orange--text mx-2">Nguyen Ngoc Nam</a></strong> | <a class="ml-1" href="https://www.paypal.me/rakujin" target="_blank"><img src="/images/Donate-PayPal-green.svg" alt="Donate"></a>
     </v-footer>
 </template>
