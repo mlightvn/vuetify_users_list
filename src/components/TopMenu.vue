34e02430ab3ae3d37976c8870cdebf67b154fb6b
Nguyen Ngoc Nam <thietlalung@yahoo.com.vn>
2020-10-21 18:41:48 +0900

    top menu

--- src/components/TopMenu.vue
+++ src/components/TopMenu.vue
@@ -46,6 +46,37 @@
 
     </v-toolbar>
 
+    <template v-slot:extension
+      v-if="true"
+    >
+      <v-tabs
+        v-model="tab"
+        align-with-title
+        background-color="orange accent-3"
+      >
+        <v-tabs-slider color="white"></v-tabs-slider>
+
+        <v-tab
+          v-for="item in items"
+          :key="item"
+        >
+          {{ item }}
+        </v-tab>
+
+        <v-tabs-items v-model="tab">
+          <v-tab-item
+            v-for="item in items"
+            :key="item"
+          >
+            <v-card flat v-if="true">
+              <v-card-text v-text="text"></v-card-text>
+            </v-card>
+          </v-tab-item>
+        </v-tabs-items>
+
+      </v-tabs>
+    </template>
+
   </v-app-bar>
 
 </template>
